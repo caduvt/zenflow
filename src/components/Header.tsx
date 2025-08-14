4351ce04b382c7b7cb769e783b6b82b320d8aab3
caduvt <carlosevtavares@gmail.com>
2025-08-13 23:41:56 -0300

    feat: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import { Trash } from 'lucide-react';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center m-4 text-xl">
+      <div>
+        <Link
+          href="/"
+          className="text-3xl font-bold cursor-pointer block underline decoration-green-400 hover:scale-105 duration-200"
+        >
+          zenflow
+        </Link>
+      </div>
+      <div>
+        <button className="cursor-pointer flex gap-2 border text-gray-700 hover:scale-105 duration-200 hover:text-red-400 border-gray-700 hover:border-red-400 rounded-md p-2 hover:shadow-cartoon">
+          <Trash />
+          <span>clear tasks</span>
+        </button>
+      </div>
+    </header>
+  );
+}
