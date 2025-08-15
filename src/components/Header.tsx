4b3b6e4188ce7e579a1ad9cc9d6ff212b341e321
caduvt <carlosevtavares@gmail.com>
2025-08-15 01:05:04 -0300

    feat: toggle dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { useTasks } from '@/hooks/TaskContext';
+import { useTheme } from '@/hooks/ThemeContext';
 import StorageService from '@/services/StorageService';
-import { Trash } from 'lucide-react';
+import { Moon, Sun, Trash } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Header() {
   const { refreshTasks } = useTasks();
+  const { isDark, toggleDarkMode } = useTheme();
 
   const handleClearTasks = () => {
     StorageService.clearTasks();
@@ -23,7 +25,14 @@ export default function Header() {
           zenflow
         </Link>
       </div>
-      <div>
+      <div className="flex gap-4">
+        <button
+          onClick={toggleDarkMode}
+          className="cursor-pointer flex gap-2 border text-gray-700 hover:scale-105 duration-200 hover:text-red-400 border-gray-700 hover:border-red-400 rounded-md p-2 hover:shadow-cartoon"
+        >
+          {isDark ? <Sun /> : <Moon />}
+          <span>{isDark ? 'light theme' : 'dark theme'}</span>
+        </button>
         <button
           onClick={handleClearTasks}
           className="cursor-pointer flex gap-2 border text-gray-700 hover:scale-105 duration-200 hover:text-red-400 border-gray-700 hover:border-red-400 rounded-md p-2 hover:shadow-cartoon"
